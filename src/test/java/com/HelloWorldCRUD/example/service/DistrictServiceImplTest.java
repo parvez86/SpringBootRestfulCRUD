6e1ce759ee34ca7b296a636175004c27dec9ab06
parvez86 <shahriarp86@gmail.com>
2022-05-22 17:28:39 +0600

    update repository test with sql query file

--- src/test/java/com/HelloWorldCRUD/example/service/DistrictServiceImplTest.java
+++ src/test/java/com/HelloWorldCRUD/example/service/DistrictServiceImplTest.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
 @SpringBootTest
 class DistrictServiceImplTest {
 
---
+++ src/test/resources/db/district1.sql
@@ -0,0 +1,8 @@
+INSERT INTO districts
+            (id,
+             name,
+             dist_code,
+             division)
+VALUES      (1, 'Sirajganj', 15, 'Rajshahi'),
+            (2, 'Narail',  22, 'Zashore')
+            ;
