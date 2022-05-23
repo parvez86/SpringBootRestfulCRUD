c759f0f68090733723c158d1ef318a1cce2b4d40
parvez86 <shahriarp86@gmail.com>
2022-05-23 16:13:44 +0600

    unit test of service layer added

--- src/test/java/com/HelloWorldCRUD/example/service/DistrictServiceImplTest.java
+++ src/test/java/com/HelloWorldCRUD/example/service/DistrictServiceImplTest.java
@@ -1,16 +1,158 @@
 package com.HelloWorldCRUD.example.service;
 
+import com.HelloWorldCRUD.example.entity.District;
+import com.HelloWorldCRUD.example.repository.DistrictRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 
 @SpringBootTest
 class DistrictServiceImplTest {
 
+    @Mock
+    private DistrictRepository districtRepository;
+
+    @InjectMocks
+    private DistrictServiceImpl districtService;
+
+    private District district1, district2;
+
     @BeforeEach
     void setUp() {
+        district1 = District.builder().
+                id(1).
+                name("Sirajganj").
+                dist_code(15).
+                division("Rajshahi").
+                build();
+
+        district2 = District.builder().
+                id(1).
+                name("Narail").
+                dist_code(22).
+                division("Zashore").
+                build();
+    }
+
+
+    // district - post method test
+    @Test
+    @DisplayName("District successfully created.")
+    void whenDistrictIsValid_thenDistrictShouldBeSaved() {
+        when(districtRepository.save(any())).thenReturn(district1);
+        District district = districtService.saveDistrict(district1);
+
+        assertThat(district).isNotNull();
+        assertThat(district).isEqualTo(district1);
+
+    }
+
+
+    @DisplayName("District not successfully created.")
+    @Test
+    void whenDistrictIsNotValid_thenDistrictShouldNotBeSaved() {
+        when(districtRepository.save(any())).thenReturn(null);
+        District district = districtService.saveDistrict(district1);
+
+        assertThat(district).isNull();
+
+    }
+
+    // district - get method test
+    @Test
+    @DisplayName("Districts are successfully found.")
+    void whenDistrictsAreValid_thenDistrictsShouldReturn() {
+        when(districtRepository.findAll(Sort.by(Sort.Direction.ASC, "division"))).thenReturn(List.of(district1, district2));
+
+        List<District> districts = districtService.getDistricts();
+        assertThat(districts).isNotNull();
+        assertThat(districts.size()).isEqualTo(2);
+        assertThat(districts.containsAll(List.of(district1,district2))).isTrue();
+    }
+
+    @Test
+    @DisplayName("Districts aren't successfully found.")
+    void whenDistrictsAreNotValid_thenDistrictsShouldNotReturn() {
+        when(districtRepository.findAll(Sort.by(Sort.Direction.ASC, "division"))).thenReturn(List.of());
+
+        List<District> districts = districtService.getDistricts();
+        assertThat(districts.isEmpty()).isTrue();
+    }
+
+    // district - get by id method test
+    @Test
+    @DisplayName("District is successfully found.")
+    void whenDistrictIdIsValid_thenDistrictShouldReturn() {
+        when(districtRepository.findById(any())).thenReturn(Optional.of(district1));
+
+        District district = districtService.getDistrict(district1.getId());
+        assertThat(district).isNotNull();
+        assertThat(district).isEqualTo(district1);
+    }
+
+    @Test
+    @DisplayName("District isn't successfully sound.")
+    void whenDistrictIdIsNotValid_thenDistrictShouldNotReturn() {
+        when(districtRepository.findById(any())).thenReturn(Optional.empty());
+
+        District district = districtService.getDistrict(district1.getId());
+        assertThat(district).isNull();
+    }
+
+    // district - update method test
+    @Test
+    @DisplayName("District is successfully updated.")
+    void whenDistrictIdIsValid_thenUpdateDistrict() {
+        when(districtRepository.findById(any())).thenReturn(Optional.of(district1));
+        when(districtRepository.save(any())).thenReturn(null);  // not reachable for this case
+
+        District district = districtService.updateDistrict(district1, district1.getId());
+        assertThat(district).isNotNull();
+        assertThat(district).isEqualTo(district1);
+    }
+
+    @Test
+    @DisplayName("District doesn't successfully updated.")
+    void whenDistrictIdIsNotValid_thenShouldNotUpdateDistrict() {
+        when(districtRepository.findById(any())).thenReturn(Optional.empty());
+        when(districtRepository.save(any())).thenReturn(district1);  // not reachable for this case
+
+        District district = districtService.updateDistrict(district1, district1.getId());
+        assertThat(district).isNull();
+    }
+
+    // district- delete method test
+    @Test
+    @DisplayName("District is successfully deleted.")
+    void whenDistrictIdIsValid_thenDeleteDistrict() {
+        when(districtRepository.findById(any())).thenReturn(Optional.of(district1));
+        doNothing().when(districtRepository).delete(any());
+
+        District district = districtService.deleteDistrict(district1.getId());
+        assertThat(district).isNotNull();
+        assertThat(district).isEqualTo(district1);
+    }
+
+    @Test
+    @DisplayName("District doesn't successfully created.")
+    void whenDistrictIdIsNotValid_thenShouldNotDeleteDistrict() {
+        when(districtRepository.findById(any())).thenReturn(Optional.empty());
+
+        District district = districtService.deleteDistrict(district1.getId());
+        assertThat(district).isNull();
     }
 }
--- src/test/java/com/HelloWorldCRUD/example/service/UserServiceImplTest.java
+++ src/test/java/com/HelloWorldCRUD/example/service/UserServiceImplTest.java
@@ -5,14 +5,20 @@ import com.HelloWorldCRUD.example.entity.User;
 import com.HelloWorldCRUD.example.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -20,10 +26,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.notIn;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith({MockitoExtension.class})
 class UserServiceImplTest {
 
     @Mock
@@ -35,104 +40,235 @@ class UserServiceImplTest {
     @Mock
     private UserDto userDto;
 
-    private User user;
+    private User user1, user2;
 
-//    @BeforeEach
-//    void setUp() {
-//        user = User.builder().
-//                id(1L).firstName("S").
-//                lastName("P").
-//                email("sp64@gmail.com").
-//                mobileNo("01518721828").
-//                isActive(true).
-//                build();
-//    }
-
-    @Test
-    @Sql("user1.sql")
-    void whenUserIsValid_thenUserShouldSaveSuccessfully() {
-        User  user = User.builder().
+    @BeforeEach
+    void setUp() {
+        user1 = User.builder().
                 id(1L).firstName("S").
                 lastName("P").
                 email("sp64@gmail.com").
                 mobileNo("01518721828").
                 isActive(true).
                 build();
-//        given(repository.save(user)).willReturn(user);
-        User saved_user = service.saveUser(user);
-//        assertThat(saved_user).isNotNull();
-        assertThat(saved_user).isEqualTo(user);
+
+        user2 = User.builder().
+                id(2L).firstName("H").
+                lastName("R").
+                email("hr64@gmail.com").
+                mobileNo("01518721828").
+                isActive(false).
+                build();
     }
+
+    // user - post
+    @Test
+    @DisplayName("User successfully created.")
+    void whenUserIsValid_thenUserShouldSaveSuccessfully() {
+        when(repository.findByEmail(any())).thenReturn(null);
+        when(repository.save(any())).thenReturn(user1);
+
+        User saved_user = service.saveUser(user1);
+        assertThat(saved_user).isEqualTo(user1);
+    }
+
 
 //    @Test
 //    void whenUserExistingEmailIsGiven_thenShouldThrowsException(){
 //        given(repository.findByEmail(user.getEmail())).willReturn(user);
 //
 //        assertThrows(EntityNotFoundException.class, () -> {service.saveUser(user);});
-//
+//updateUser
 //        verify(repository, never()).save(any(User.class));
 //    }
 
     @Test
+    @DisplayName("User not successfully created.")
     void whenUserExistingEmailIsGiven_thenNoUserShouldSave(){
-        given(repository.findByEmail(user.getEmail()) ).willReturn(user);
+        when(repository.findByEmail(any())).thenReturn(user1);
+        User saved_user = service.saveUser(user1);
+        assertThat(saved_user).isNull();
+    }
 
-        if(repository.findByEmail(user.getEmail()) == null){
-            repository.save(user);
-        }
-
-        assertThrows(EntityNotFoundException.class, () -> {service.saveUser(user);});
-
-        verify(repository, never()).save(any(User.class));
+    // user - get
+    @Test
+    @DisplayName("User are successfully found.")
+    void whenUsersAreFound_thenUsersShouldReturn() {
+        when(repository.findAll(Sort.by(Sort.Direction.ASC, "firstName"))).thenReturn(List.of(user1, user2));
+        List<User> users = service.getUsers();
+        assertThat(users.size()).isEqualTo(2);
+        assertThat(users.containsAll(List.of(user1, user2))).isTrue();
     }
 
     @Test
-    @Disabled
-    void saveUsers() {
+    @DisplayName("User aren't successfully found.")
+    void whenUsersAreNotFound_thenEmptyShouldReturn() {
+        when(repository.findAll(Sort.by(Sort.Direction.ASC, "firstName"))).thenReturn(List.of());
+        List<User> users = service.getUsers();
+        assertThat(users).isNull();
+    }
+
+    // user - get by id
+    @Test
+    @DisplayName("User is successfully found.")
+    void whenUserIdIsValid_thenUsersShouldReturn() {
+        long user_id = 1L;
+        when(repository.findById(user1.getId())).thenReturn(Optional.of(user1));
+        User filtered_user= service.getUserById(user_id);
+        assertThat(filtered_user.getId()).isEqualTo(user_id);
+        assertThat(filtered_user).isEqualTo(user1);
     }
 
     @Test
-    @Disabled
-    void getUsers() {
+    @DisplayName("User isn't successfully found.")
+    void whenUserIdIsNotValid_thenNullsShouldReturn() {
+        long user_id = 1L;
+        when(repository.findById(user1.getId())).thenReturn(Optional.empty());
+        User filtered_user= service.getUserById(user_id);
+        assertThat(filtered_user).isNull();
+    }
+
+    // user - get by first name
+    @Test
+    @DisplayName("User successfully found by first name.")
+    void whenFirstNameIsValid_thenUserShouldReturn() {
+        String user_first_name = "S";
+        when(repository.findByFirstName(user1.getFirstName())).thenReturn(List.of(user1));
+
+        List<User> users = service.getUserByFirstName(user_first_name);
+        assertThat(users.size()).isEqualTo(1);
+        assertThat(users.contains(user1)).isTrue();
     }
 
     @Test
-    @Disabled
-    void getUserById() {
+    @DisplayName("User isn't successfully created by first name.")
+    void whenFirstNameIsNotValid_thenNullShouldReturn() {
+        String user_first_name = "P";
+        when(repository.findByFirstName(any())).thenReturn(List.of());
+
+        List<User> users = service.getUserByFirstName(user_first_name);
+        assertThat(users).isNull();
     }
 
     @Test
-    @Disabled
-    void getUserByFirstName() {
+    @DisplayName("User is successfully found by last name.")
+    void whenLastNameIsValid_thenUserShouldReturn() {
+        String user_last_name = "P";
+        when(repository.findByLastName(user1.getLastName())).thenReturn(List.of(user1));
+
+        List<User> users = service.getUserByLastName(user_last_name);
+        assertThat(users.size()).isEqualTo(1);
+        assertThat(users.contains(user1)).isTrue();
     }
 
     @Test
-    @Disabled
-    void getUserByLastName() {
+    @DisplayName("User isn't successfully found by last name.")
+    void whenLastNameIsNotValid_thenNullShouldReturn() {
+        String user_last_name = "S";
+        when(repository.findByLastName(any())).thenReturn(List.of());
+
+        List<User> users = service.getUserByLastName(user_last_name);
+        assertThat(users).isNull();
     }
 
     @Test
-    @Disabled
-    void getUserByEmail() {
+    @DisplayName("User is successfully found by email.")
+    void whenEmailIsValid_thenUserShouldReturn() {
+        String user_email = "sp64@gmail.com";
+        when(repository.findByEmail(any())).thenReturn(user1);
+
+        User user = service.getUserByEmail(user_email);
+        assertThat(user).isNotNull();
+        assertThat(user).isEqualTo(user1);
     }
 
     @Test
-    @Disabled
-    void deleteUser() {
+    @DisplayName("User isn't successfully found by email.")
+    void whenEmailIsNotValid_thenNullShouldReturn() {
+        String user_email = "sp64@gmail.com";
+        when(repository.findByEmail(any())).thenReturn(null);
+
+        User user = service.getUserByEmail(user_email);
+        assertThat(user).isNull();
     }
 
     @Test
-    @Disabled
-    void updateUser() {
+    @DisplayName("User is successfully deleted.")
+    void whenUserIdIsValid_thenUserShouldBeDeleted() {
+        long user_id = 1L;
+        when(repository.findById(user1.getId())).thenReturn(Optional.of(user1));
+        doNothing().when(repository).delete(any());
+        User filtered_user= service.deleteUser(user_id);
+        assertThat(filtered_user.getId()).isEqualTo(user_id);
+        assertThat(filtered_user).isEqualTo(user1);
+    }
+
+    @Test@DisplayName("User isn't successfully deleted.")
+
+    void whenUserIdIsNotValid_thenNoUserShouldBeDeleted() {
+        long user_id = 1L;
+        when(repository.findById(user1.getId())).thenReturn(Optional.empty());
+        User filtered_user= service.deleteUser(user_id);
+        assertThat(filtered_user).isNull();
+    }
+
+
+
+    @Test
+    @DisplayName("User is successfully updated.")
+    void whenUserIdIsValid_thenUpdateUser() {
+        when(repository.findById(any())).thenReturn(Optional.of(user1));
+        when(repository.save(any())).thenReturn(user1);
+
+        User updated_user = service.updateUser(user1, user1.getId());
+        assertThat(updated_user).isNotNull();
+        assertThat(updated_user).isEqualTo(user1);
     }
 
     @Test
-    @Disabled
-    void deactivateUser() {
+    @DisplayName("User isn't successfully updated.")
+    void whenUserIdIsNotValid_thenNotUpdateUser() {
+        when(repository.findById(any())).thenReturn(Optional.empty());
+
+        User updated_user = service.updateUser(user1, user1.getId());
+        assertThat(updated_user).isNull();
     }
 
     @Test
-    @Disabled
-    void getActiveUsers() {
+    @DisplayName("User is successfully deactivated.")
+    void whenUserIdIsValid_thenDeactivateUser() {
+        when(repository.findById(user1.getId())).thenReturn(Optional.of(user1));
+        when(repository.save(any())).thenReturn(user1);
+
+        User user = service.deactivateUser(user1.getId());
+        assertThat(user).isNotNull();
+        assertThat(user).isEqualTo(user1);
+    }
+
+    @Test
+    @DisplayName("User doesn't successfully deactivated.")
+    void whenUserIdIsNotValid_thenNotDeactivateUser() {
+        when(repository.findById(any())).thenReturn(Optional.empty());
+
+        User updated_user = service.deactivateUser(user1.getId());
+        assertThat(updated_user).isNull();
+    }
+
+    @Test@DisplayName("Active Users are successfully found.")
+
+    void whenActiveUserFound_thenActiveUsersShouldReturn() {
+        when(repository.findByIsActiveTrue()).thenReturn(List.of(user1));
+
+        List<User> user = service.getActiveUsers();
+        assertThat(user).isNotNull();
+        assertThat(user).isEqualTo(List.of(user1));
+    }
+
+    @Test
+    @DisplayName("No active user found")
+    void whenNoActiveUserFound_thenNullShouldReturn() {
+        when(repository.findByIsActiveTrue()).thenReturn(List.of());
+        List<User> updated_user = service.getActiveUsers();
+        assertThat(updated_user).isNull();
     }
 }
